93d5f7a06c22cc1a5056f6667cf8261968500f76
BuffsOverNexus <rgoldbe4@uncc.edu>
2021-03-26 19:08:52 -0400

    Models - Committed working models as of now. Also added example code in app.py

--- app.py
+++ app.py
@@ -1,16 +1,30 @@
-from flask import Flask
+import hashlib
+
+from flask import Flask, jsonify
 import database
-import models
+from models import *
 
 app = Flask(__name__)
-database.Base.metadata.create_all(database.postgres_engine)
 
 # Create a database with the correct models.
+database.Base.metadata.create_all(database.postgres_engine)
+
+session = database.Session()
+license = session.query(License).filter_by(id=1).first()
+password = hashlib.sha256("password1".encode()).digest()
+admins = license.administrators
+
+admin = session.query(Administrator).filter_by(id=2).first()
+
+print(admin.first_name, admin.license.accounts)
+
+session.commit()
+session.close()
 
 
 @app.route('/')
 def hello_world():
-  return 'Hello World'
+  return jsonify()
 
 
 if __name__ == '__main__':
