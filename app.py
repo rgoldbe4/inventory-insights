7bceec5aa96d9bd22e8ba811a80c87e1faa677a2
BuffsOverNexus <rgoldbe4@uncc.edu>
2021-03-26 19:09:31 -0400

    Removed excess code to move to Master

--- app.py
+++ app.py
@@ -9,18 +9,6 @@ app = Flask(__name__)
 # Create a database with the correct models.
 database.Base.metadata.create_all(database.postgres_engine)
 
-session = database.Session()
-license = session.query(License).filter_by(id=1).first()
-password = hashlib.sha256("password1".encode()).digest()
-admins = license.administrators
-
-admin = session.query(Administrator).filter_by(id=2).first()
-
-print(admin.first_name, admin.license.accounts)
-
-session.commit()
-session.close()
-
 
 @app.route('/')
 def hello_world():
