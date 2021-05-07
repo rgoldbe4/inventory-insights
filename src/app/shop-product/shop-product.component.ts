f7f49228f4bb76a9954d43e8f9d9ceea7af963df
azadoroz <azadoroz@uncc.edu>
2021-05-06 21:03:23 -0400

    Updated Store Products page and created Store Product page(Not Complete).

---
+++ src/app/shop-product/shop-product.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {ActivatedRoute} from '@angular/router';
+
+@Component({
+  selector: 'app-shop-product',
+  templateUrl: './shop-product.component.html',
+  styleUrls: ['./shop-product.component.css']
+})
+export class ShopProductComponent implements OnInit {
+
+  constructor(private http: HttpClient, private route: ActivatedRoute) { }
+
+  id: number;
+  item: any = {};
+
+  // When the user clicks the Save button
+  save(): void {
+    // Make sure all of the fields are filled in.
+    if (this.item.name && this.item.price && this.item.cost &&
+      this.item.description && this.item.instock && this.item.category) {
+      // Send the item to the database
+      this.http.post<any>('http://127.0.0.1:5000/items/save', { item: this.item }).subscribe(result => {
+        this.item = result.item;
+      });
+    }
+  }
+
+  // When the user clicks the Discontinue button
+  discontinue(): void {
+    this.http.post<any>('http://127.0.0.1:5000/items/discontinue',{ id: this.item.id, license_id: this.item.license.id }).subscribe(result => {
+      console.log(result.item);
+      this.item = result.item;
+    });
+  }
+
+  ngOnInit(): void {
+    this.route.params.subscribe( params => {
+      this.id = params['id'];
+      // Retrieves the current item based off the given ID.
+      this.http.post<any>('http://127.0.0.1:5000/items/item', { id: this.id }).subscribe(result => {
+          this.item = result.item;
+      });
+    });
+  }
+
+}
--- src/app/shop-products/shop-products.component.ts
+++ src/app/shop-products/shop-products.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-shop-products',
@@ -7,9 +9,70 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShopProductsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient, private route: ActivatedRoute) { }
+
+  // All items to display on the page.
+  items: any = [];
+  showDiscontinued: boolean = false; // Whether or not to show discontinued items.
+
+  // Pagination
+  displayedItems: any[] = []; // The items to display on the page.
+  maxItems: number = 10; // The maximum number of items to display per page.
+  numItems: number = 10; // The number of items in total
+  pages: number[] = []; // The number of pages to display, in an array.
+  currentPage: number = 0;
+  maxPages: number = 0;
+
+  // Toggle displaying discontinued items
+  toggleDiscontinued(): void {
+    this.showDiscontinued = !this.showDiscontinued;
+  }
+
+  // Update the displayedItems array with correct elements upon clicked.
+  getDisplayedItems(start: number, end: number): void {
+    this.currentPage = start;
+    start = start * this.maxItems;
+    end = end * this.maxItems;
+    this.displayedItems = [];
+    for (let i = start; i < end; i++) {
+      if (this.items[i] != undefined)
+        this.displayedItems.push(this.items[i]);
+    }
+  }
+
+  // This updates the pagination variables on the page any time it updates.
+  updatePagination(result: any) : void {
+    this.numItems = result['items'].length;
+    this.maxPages = Math.ceil(this.numItems / this.maxItems);
+    // Default from 0 to 10.
+    this.getDisplayedItems(this.currentPage, this.currentPage + 1);
+    // Maps from 0 to numPages (0 to 10 would be [0,1,2,3,...,10])
+    this.pages = Array(this.maxPages).map((x, i)=>i);
+  }
+
+  // Discontinue a product.
+  discontinue(id: number) : void {
+    let license_id = localStorage.getItem("admin_license_id");
+    this.http.post<any>('http://127.0.0.1:5000/items/discontinue',{ id: id, license_id: license_id })
+      .subscribe(discontinue_result => {
+        // Reacquire all of the items from the list.
+        this.http.post<any>('http://127.0.0.1:5000/items/all', { license_id: license_id })
+          .subscribe(result => {
+            this.items = result.items;
+            this.updatePagination(result);
+          }
+        );
+
+      }
+    );
+  }
 
   ngOnInit(): void {
+    let license_id = localStorage.getItem("admin_license_id");
+    this.http.post<any>('http://127.0.0.1:5000/items/all', { license_id: license_id }).subscribe(result => {
+      this.items = result.items;
+      this.updatePagination(result);
+    });
   }
 
 }
