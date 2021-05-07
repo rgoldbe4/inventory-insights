27e887f830ea04ec43862cf9f599f49d00ff010a
Robert Hewson <rhewson@uncc.edu>
2021-05-07 13:30:42 -0400

    Monthly sales graph output no longer takes a minute

--- backend/graphs.py
+++ backend/graphs.py
@@ -1,9 +1,7 @@
 from backend.models import *
 
 def monthlySales(session, id):
-    sales = session.query(Order).all()
-    carts = session.query(Cart).all()
-    print(len(carts))
+    sales = session.query(association_order_table).filter_by(item_id = id).all()
     jan = 0
     feb = 0
     mar = 0
@@ -17,31 +15,30 @@ def monthlySales(session, id):
     nov = 0
     dec = 0
     for sale in sales:
-        for item in sale.items:
-            if item.id == id:
-                if sale.date.month == 1:
-                    jan +=1
-                elif sale.date.month == 2:
-                    feb +=1
-                elif sale.date.month == 3:
-                    mar +=1 
-                elif sale.date.month == 4:
-                    apr +=1
-                elif sale.date.month == 5:
-                    may +=1
-                elif sale.date.month == 6:
-                    jun +=1
-                elif sale.date.month == 7:
-                    jul +=1
-                elif sale.date.month == 8:
-                    aug +=1
-                elif sale.date.month == 9:
-                    sep +=1
-                elif sale.date.month == 10:
-                    oct +=1 
-                elif sale.date.month == 11:
-                    nov +=1
-                elif sale.date.month == 12:
-                    dec +=1
-    print(len(sales))
+        myOrder = session.query(Order).filter_by(id = sale.order_id).first()
+        if myOrder.date.month == 1:
+            jan +=1
+        elif myOrder.date.month == 2:
+            feb +=1
+        elif myOrder.date.month == 3:
+            mar +=1 
+        elif myOrder.date.month == 4:
+            apr +=1
+        elif myOrder.date.month == 5:
+            may +=1
+        elif myOrder.date.month == 6:
+            jun +=1
+        elif myOrder.date.month == 7:
+            jul +=1
+        elif myOrder.date.month == 8:
+            aug +=1
+        elif myOrder.date.month == 9:
+            sep +=1
+        elif myOrder.date.month == 10:
+            oct +=1 
+        elif myOrder.date.month == 11:
+            nov +=1
+        elif myOrder.date.month == 12:
+            dec +=1
+    
     return jan,feb,mar,apr,may,jun,jul,aug,sep,oct,nov,dec
