4e34465230fa1f273b106bd2e404b7f52514f023
Robert Hewson <rhewson@uncc.edu>
2021-05-07 17:33:57 -0400

    recommender system works now (hopefully)

--- backend/recommend.py
+++ backend/recommend.py
@@ -15,9 +15,10 @@ def recommendItem(session, cart):
     categoryItems = session.query(Item).filter_by(category = category).all()
     allItems = session.query(Item).all()
     evList = {}
-    recList = {}
+    recList = []
     for item in categoryItems:
-        if cart.items.count(item) > 1:
+        occurences = len(session.query(association_cart_table).filter_by(item_id = item.id, cart_id = cart.id).all())
+        if occurences == 0:
             recs = session.query(Recommendation).filter_by(recommended = item).all()
             suc = 0
             total = 0
@@ -31,27 +32,26 @@ def recommendItem(session, cart):
                 evList[item] = ev
     for item in allItems:
         if len(evList) < 2:
-            if categoryItems.count(item) > 0:
-                recs = session.query(Recommendation).filter_by(recommended = item).all()
-                suc = 0
-                total = 0
-                if len(recs) > 0:
-                    for rec in recs:
-                        if rec.success:
-                            suc += 1
-                        total += 1
-                    profit = item.price - item.cost
-                    ev = (profit*suc)/total
-                    evList[item] = ev
+            if item.category != category:
+                occurences = len(session.query(association_cart_table).filter_by(item_id = item.id, cart_id = cart.id).all())
+                if occurences == 0:
+                    recs = session.query(Recommendation).filter_by(recommended = item).all()
+                    suc = 0
+                    total = 0
+                    if len(recs) > 0:
+                        for rec in recs:
+                            if rec.success:
+                                suc += 1
+                            total += 1
+                        profit = item.price - item.cost
+                        ev = (profit*suc)/total
+                        evList[item] = ev
         else:
             break
     evList = sorted(evList, key = evList.get)
-    for item in cart.items:
-        print(item.name)
-    print("_______________")
-    for item in evList:
-        print(item.name)
-
+    recList.append(evList[0])
+    recList.append(evList[1])
+    return recList
     
     
 
