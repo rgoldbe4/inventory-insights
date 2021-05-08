21d984242391ed95e4110e66e5e15bb51693a8c0
rgoldbe4 <rgoldbe4@uncc.edu>
2021-05-07 22:25:29 -0400

    Updated item-info graph to be bigger in size.

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -10,8 +10,40 @@ export class AdminComponent implements OnInit {
 
   constructor(private http: HttpClient) { }
 
+  costs = [];
+  sales = [];
+
   ngOnInit(): void {
 
   }
+
+  public chartType: string = 'line';
+
+  public chartDatasets: Array<any> = [
+    { data: this.costs, label: 'Total Cost By Month' },
+    { data: this.sales, label: 'Total Sales By Month'}
+  ];
+
+  public chartLabels: Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September',
+                                    'October', 'November', 'December'];
+
+  public chartColors: Array<any> = [
+    {
+      backgroundColor: 'rgba(105, 0, 132, .2)',
+      borderColor: 'rgba(200, 99, 132, .7)',
+      borderWidth: 2,
+    },
+    {
+      backgroundColor: 'rgba(0, 137, 132, .2)',
+      borderColor: 'rgba(0, 10, 130, .7)',
+      borderWidth: 2,
+    }
+  ];
+
+  public chartOptions: any = {
+    responsive: true
+  };
+  public chartClicked(e: any): void { }
+  public chartHovered(e: any): void { }
 
 }
